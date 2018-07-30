0a404b3a0822869fb6e9e3d89410685fb7f5fb36
Lewis <lewismorgan1995@icloud.com>
2018-07-30 17:17:27 -0400

    Update daily launchctrl for network check

--- .cron/daily.sh
+++ .cron/daily.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 PATH=/usr/local/bin:${PATH}
 
+while [ "${NETWORKUP}" != "-YES-" ]
+do
+  sleep 300
+  case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
+    [23]) NETWORKUP="-YES-";;
+    5) NETWORKUP="-NO-";;
+    *) NETWORKUP="-MAYBE-";;
+  esac
+done
+
 # Update Homebrew
 brew update
 brew upgrade
