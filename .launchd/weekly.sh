0c7acf50125f266ec8112909fd401395d4083505
Lewis <lewismorgan1995@icloud.com>
2018-12-19 18:06:50 -0500

    Update powerlevel9k weekly

--- .launchd/weekly.sh
+++ .launchd/weekly.sh
@@ -3,5 +3,8 @@ PATH=/usr/local/bin:${PATH}
 
 # Cleanup Homebrew!
 DATE=$(date +%d-%m-%Y" "%H:%M:%S )
-echo "🕥 $DATE | Performing weekly Homebrew cleanup 🍺."
+echo "🕥 $DATE | Performing weekly tasks 🍺."
 brew cleanup
+
+echo "Updating powerlevel9k repository"
+git --git-dir ~/.oh-my-zsh/custom/themes/powerlevel9k pull
