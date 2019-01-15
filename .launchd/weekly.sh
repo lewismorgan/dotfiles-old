54cafe8ba7c91dc0d9e546eb02f2f67687a05988
Lewis <lewismorgan1995@icloud.com>
2019-01-15 18:51:28 -0500

    Fix powerlevel9k not updating :bug:

--- .launchd/weekly.sh
+++ .launchd/weekly.sh
@@ -7,4 +7,4 @@ echo "🕥 $DATE | Performing weekly tasks 🍺."
 brew cleanup
 
 echo "Updating powerlevel9k repository"
-git --git-dir ~/.oh-my-zsh/custom/themes/powerlevel9k pull
+git --git-dir ~/.oh-my-zsh/custom/themes/powerlevel9k/.git pull
